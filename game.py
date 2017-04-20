d32a9e5a7dfbac87ca0a326ce9f9a864183718ce
Ian Booton <ianbooton@Ians-MacBook-Pro.local>
2017-04-20 13:57:59 -0400

    our hero can move

--- game.py
+++ game.py
@@ -8,13 +8,58 @@ screen = {
 	"width": 480,
 }
 
+keys = {
+	"right": 275,
+	"left": 276,
+	"up": 273,
+	"down": 274
+}
+
+hero = {
+	'x': 100,
+	'y': 100,
+	'speed': 20
+}
 screen_size = (screen["height"], screen["width"])
 pygame_screen = pygame.display.set_mode(screen_size)
 pygame.display.set_caption("Goblin Chase")
+backgroung_image = pygame.image.load('images/background.png')
+hero_image =pygame.image.load('images/hero.png')
+
+# //////////////MAIN GAME LOOP////////////////
+# //////////////MAIN GAME LOOP////////////////
+# //////////////MAIN GAME LOOP////////////////
 
 
 game_on = True
 while game_on: 
+	# ---EVENTS!---
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
-			game_on = False
+			game_on = False	
+		elif event.type == pygame.KEYDOWN:
+			print event.key
+			if event.key == keys['up']:
+				print "User pressed up!"
+				hero['y'] -= hero['speed']
+			elif event.key == keys['down']:
+				print "User pressed down!"
+				hero['y'] += hero['speed']
+			elif event.key == keys['right']:
+				print "User pressed right!"
+				hero['x'] += hero['speed']
+			elif event.key == keys['left']:
+			 print "User pressed down!"
+			 hero['x'] -= hero['speed']
+	# RENDER
+	# blit takes 2 arguments
+	# 1. What?
+	# 2. Where?
+	pygame_screen.blit(backgroung_image, [0,0])
+
+	# draw the hero 
+	pygame_screen.blit(hero_image, [hero['x'], hero['y']])
+
+
+	# clear screen for the next time
+	pygame.display.flip()
