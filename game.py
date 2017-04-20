703ff449ff92563dcf251056ca1a47752cc23901
Ian Booton <ianbooton@Ians-MacBook-Pro.local>
2017-04-20 13:15:45 -0400

    pre-lunch work

---
+++ game.py
@@ -0,0 +1,20 @@
+import pygame
+
+# in order to use py game, we have top use the init method
+pygame.init()
+
+screen = {
+	"height": 512,
+	"width": 480,
+}
+
+screen_size = (screen["height"], screen["width"])
+pygame_screen = pygame.display.set_mode(screen_size)
+pygame.display.set_caption("Goblin Chase")
+
+
+game_on = True
+while game_on: 
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			game_on = False
